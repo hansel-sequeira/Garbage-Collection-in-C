a5d2c76d484f9030a31642335e80f4ecaecb016f
Hansel Sequeira <sequeirahansel@gmail.com>
2024-04-13 23:32:20 +0530

    completed malloc implementation

--- cuslib.c
+++ cuslib.c
@@ -27,7 +27,14 @@ Chunk_List freed_chunk_list = {
         .size = sizeof(heap),
         .start = heap}};
 
-int chunk_comparator(const void *a, const void *b)
+int chunk_comparator_size(const void *a, const void *b)
+{
+    const Chunk *a_chunk = a;
+    const Chunk *b_chunk = b;
+    return (a_chunk->size - b_chunk->size);
+}
+
+int chunk_comparator_address(const void *a, const void *b)
 {
     const Chunk *a_chunk = a;
     const Chunk *b_chunk = b;
@@ -39,11 +46,30 @@ int chunk_list_search(Chunk_List *list, void *ptr)
     const Chunk key_chunk = {
         .start = ptr};
     Chunk *ret = bsearch(&key_chunk, list->chunks, list->chunk_count, sizeof(list->chunks[0]),
-                         chunk_comparator);
+                         chunk_comparator_address);
     if (ret == NULL)
         return -1;
     assert(ret >= list->chunks);
     return ((ret - list->chunks));
+}
+
+void chunk_list_compaction(Chunk_List *list)
+{
+    Chunk_List temp = {0};
+    for (size_t i = 0; i < list->chunk_count; i++)
+    {
+        if (temp.chunk_count == 0)
+            temp.chunks[temp.chunk_count++] = list->chunks[i];
+        else
+        {
+            if (temp.chunks[temp.chunk_count - 1].start + temp.chunks[temp.chunk_count - 1].size == list->chunks[i].start)
+                temp.chunks[temp.chunk_count - 1].size += list->chunks[i].size;
+            else
+                temp.chunks[temp.chunk_count++] = list->chunks[i];
+        }
+    }
+
+    *list = temp;
 }
 
 void chunk_list_free(Chunk_List *list, size_t index)
@@ -71,8 +97,6 @@ void chunk_list_insert(Chunk_List *list, void *ptr, size_t size)
     list->chunks[list->chunk_count].start = ptr;
     list->chunks[list->chunk_count].size = size;
 
-    // sort on insertion based on pointer value
-
     for (size_t i = list->chunk_count; i > 0 &&
                                        list->chunks[i].start < list->chunks[i - 1].start;
          i--)
@@ -93,16 +117,14 @@ void *custom_alloc(size_t required_size)
     for (size_t i = 0; i < freed_chunk_list.chunk_count; i++)
     {
         if (freed_chunk_list.chunks[i].size >= required_size)
-        { // first-fit allocation
+        {
             Chunk victim_free_chunk = freed_chunk_list.chunks[i];
             void *ptr = victim_free_chunk.start;
             int remaining_size = victim_free_chunk.size - required_size;
             chunk_list_insert(&allocated_chunk_list, ptr, required_size);
             chunk_list_free(&freed_chunk_list, i);
             if (remaining_size > 0)
-            {
                 chunk_list_insert(&freed_chunk_list, ptr + required_size, remaining_size);
-            }
 
             return ptr;
         }
@@ -118,23 +140,23 @@ void custom_free(void *ptr)
     assert(idx >= 0);
     // remove this from the allocated chunk list and insert in the freed chunk list
     chunk_list_insert(&freed_chunk_list, allocated_chunk_list.chunks[idx].start, allocated_chunk_list.chunks[idx].size);
+    qsort(&(freed_chunk_list.chunks), freed_chunk_list.chunk_count, sizeof(freed_chunk_list.chunks[0]), chunk_comparator_size);
+    chunk_list_compaction(&freed_chunk_list);
     chunk_list_free(&allocated_chunk_list, idx);
 }
 
 int main()
 {
-    // for (int i = 0; i < 10; i++)
-    // {
-    //     void *ret = custom_alloc(i);
-    //     if (i % 2 == 0)
-    //         custom_free(ret);
-    // }
-    void *p1 = custom_alloc(1);
-    void *p2 = custom_alloc(2);
-    custom_free(p1);
-    custom_free(p2);
 
-    custom_alloc(3);
+    void *ptr[10] = {0};
+    for (int i = 0; i < 10; i++)
+    {
+        ptr[i] = custom_alloc(i);
+    }
+
+    custom_free(ptr[7]);
+    custom_free(ptr[6]);
+
     printf("Allocated chunks..\n");
     chunk_list_dump(&allocated_chunk_list);
     printf("Freed chunks..\n");
